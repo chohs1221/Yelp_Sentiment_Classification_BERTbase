895feffbc19862e2babbdcf9ad701d33d8525c54
chohs1221 <hscho1221@naver.com>
2022-03-31 21:43:35 +0900

    pull files

--- compute.py
+++ compute.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 
 
@@ -13,20 +12,4 @@ output: accuracy
 
 
 def compute_acc(predictions, target_labels):
-=======
-import numpy as np
-
-
-###################################################################
-'''
-returns accuracy
-< compute_acc >
-input: predictions, target_labels
-output: accuracy
-'''
-###################################################################
-
-
-def compute_acc(predictions, target_labels):
->>>>>>> a276db7324688919f66b41a3801e2fc014889596
     return (np.array(predictions) == np.array(target_labels)).mean()
--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import re
 
 
@@ -14,20 +13,3 @@ output: data in regular expression
 
 def regular(data):
     return [' '.join(re.sub('[-=+,#/$\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', '', sent.replace('_num_', ' ').replace('\n', ' ')).split()) for sent in data]
-=======
-import re
-
-
-###################################################################
-'''
-regular expression
-< regular >
-input: data
-output: data in regular expression
-'''
-###################################################################
-
-
-def regular(data):
-    return [' '.join(re.sub('[-=+,#/$\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', '', sent.replace('_num_', ' ').replace('\n', ' ')).split()) for sent in data]
->>>>>>> a276db7324688919f66b41a3801e2fc014889596
